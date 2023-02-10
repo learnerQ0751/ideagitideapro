c5bdbaef81a10875ba7e7abc76fd0d4833a22f71
learnerQ0751 <1561715971@qq.com>
2023-02-10 15:50:08 +0800

    这是我在idea第六次使用git的demo--》master1

--- src/main/java/com/qtua/gittest/gitdemo/GitTest.java
+++ src/main/java/com/qtua/gittest/gitdemo/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("Hello Git3");
         System.out.println("master test1");
         System.out.println("hot-fix test");
+        System.out.println("master test2");
     }
 }
